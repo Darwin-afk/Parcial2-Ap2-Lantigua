ef8c8604d05b9b5f4f34c67f63367f07cf4c0460
Darwin-afk <darwin_lantigua@ucne.edu.do>
2020-07-07 20:30:25 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Parcial2_Ap2_Lantigua.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
